4497c30634029925a0332d5ed2089db24a0a5d11
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-20 16:03:17 +0530

    Create basictasks.sh

---
+++ basictasks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Purpose: To run basic tasks
+# Date:
+# Authour:
+
+
+echo “This Script runs basic tasks”
+
+echo
+pwd
+echo
+ls
+echo
+whoami
+echo
+date
+echo
+cal
+echo
+touch a b c
