320e7387fff9e6be6f2c6b3408031fa00c33f0c3
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-20 16:04:03 +0530

    Create admintasks.sh

---
+++ admintasks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script is to display simple output
+# Purpose: To train on bash scripting
+# creation date:
+# Modified date:
+# Author:
+
+Farooq="Good boy"
+echo $Farooq
+echo
+date
+echo
+df -h
+echo
