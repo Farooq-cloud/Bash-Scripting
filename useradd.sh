649b7ca19233448fb221814ddf96f2967ebc997b
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2023-10-24 18:44:37 +0530

    Create useradd.sh

---
+++ useradd.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Aurthor : Farooq
+echo "Please provide a username"
+read user
+echo
+useradd $user
+echo "User has been created"
+==================================================================================================================================================
+
+#!/bin/bash
+#Aurthor : TTT
+echo "Please provide a username"
+read user
+echo
+grep -q $user /etc/passwd
+if [ $? -eq 0 ]
+then
+echo "error – user $user already exists"
+echo "please define another user"
+echo
+exit 0
+fi	
+useradd $user
+echo "$user has been created succesfully"
+
+==================================================================================================================================================
+
+#!/bin/bash
+echo "Please provide a username"
+read user
+echo
+grep -q $user /etc/passwd
+if [ $? -eq 0 ]
+then
+echo "error – user $user already exists"
+echo "please define another user"
+echo
+exit 0
+fi
+echo "Please add description for the user"
+read desc
+echo
+echo "Please specify the password inactivity period (in days):"
+read inactivity_period
+echo
+
+
